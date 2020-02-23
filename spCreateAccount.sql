259b614b5c3e5c47c3cbb450ae4200483d579088
Isabel Shiff <isabelshiff@gmail.com>
2020-02-23 12:40:01 -0500

    initial stored procedures

--- spCreateAccount.sql
+++ spCreateAccount.sql
@@ -8,4 +8,42 @@ CREATE PROCEDURE SP_Create_account
     @affiliation VARCHAR(256),
     @school VARCHAR(256),
     @password VARCHAR(256),
-    )
+    @license_no INTEGER,
+    @license_plate_no VARCHAR(10),
+    @plate_state VARCHAR(3))
+
+AS
+BEGIN
+
+SET NOCOUNT ON
+
+INSERT INTO [dbo].[User]
+    ([netid],
+    [name],
+    [duke_email],
+    [phone_number],
+    [affiliation],
+    [school],
+    [password])
+    VALUES
+    (@netid,
+    @name,
+    @duke_email,
+    @phone_number,
+    @affiliation,
+    @school,
+    @password);
+
+IF @license_no IS NOT NULL
+    INSERT INTO [dbo].[Driver]
+        ([netid],
+        [license_no],
+        [license_plate_no],
+        [plate_state])
+        VALUES
+        (@netid,
+        @license_no,
+        @license_plate_no,
+        @plate_state);
+
+GO
