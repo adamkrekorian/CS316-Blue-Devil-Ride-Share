d5bd05d24170686e63b08f1d6fd0399b69ab43d0
Isabel Shiff <isabelshiff@gmail.com>
2020-02-23 14:59:25 -0500

    meredith's edits to create

--- create.sql
+++ create.sql
@@ -1,27 +1,27 @@
-CREATE TABLE User
+CREATE TABLE rideshare_user
 (netid VARCHAR(7) NOT NULL UNIQUE PRIMARY KEY,
  name VARCHAR(256) NOT NULL,
  duke_email VARCHAR(256) NOT NULL,
  phone_number INTEGER NOT NULL,
  affiliation VARCHAR(256) NOT NULL CHECK(affiliation IN ('Undergraduate', 'Graduate')),
  school VARCHAR(256) NOT NULL CHECK(school IN ('Trinity', 'Pratt', 'Nicolas', 'Law', 'Fuqua', 
-                'Nursing', 'Medicine', 'Other')))
- password VARCHAR(256) NOT NULL);
+                'Nursing', 'Medicine', 'Other')),
+ password VARCHAR(256) NOT NULL);
 
 CREATE TABLE Driver
-(netid VARCHAR(7) NOT NULL PRIMARY KEY REFERENCES User(netid),
+(netid VARCHAR(7) NOT NULL PRIMARY KEY REFERENCES rideshare_user(netid),
  license_no INTEGER NOT NULL,
  license_plate_no VARCHAR(10) NOT NULL,
  plate_state VARCHAR(3) NOT NULL);
 
 CREATE TABLE Ride
-(ride_no INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+(ride_no SERIAL PRIMARY KEY,
  origin VARCHAR(100) NOT NULL,
  destination VARCHAR(100) NOT NULL CHECK(destination <> origin), -- check this -- if this doesnt work make a trigger
  driver_netid VARCHAR(7) NOT NULL REFERENCES Driver(netid),
  date DATE NOT NULL, 
- earliest_time TIME CHECK(earliest_time > 00:00:00 AND earliest_time < latest_time),
- latest_time TIME CHECK (latest_time < 23:59:59 AND latest_time > earliest_time),
+ earliest_time TIME CHECK(earliest_time > '00:00:00' AND earliest_time < latest_time),
+ latest_time TIME CHECK (latest_time < '23:59:59' AND latest_time > earliest_time),
  seats_available INTEGER NOT NULL, 
  gas_price INTEGER,
  comments VARCHAR(500));
@@ -32,8 +32,8 @@ CREATE TABLE Ride
   seats_needed INTEGER NOT NULL,
   note VARCHAR(150), 
   PRIMARY KEY(rider_netid, ride_no),
-  FOREIGN KEY rider_netid REFERENCES User(netid),
-  FOREIGN KEY ride_no REFERENCES Ride(ride_no));
+  FOREIGN KEY (rider_netid) REFERENCES rideshare_user(netid),
+  FOREIGN KEY (ride_no) REFERENCES Ride(ride_no));
 
 CREATE TABLE Post -- don't think we need this table
 (driver_netid VARCHAR(7) NOT NULL REFERENCES Driver(netid),
@@ -61,9 +61,10 @@ CREATE TRIGGER TG_Driver_own_rider
 
 CREATE FUNCTION TF_Not_enough_seats() RETURNS TRIGGER AS $$
 BEGIN
-  WHEN Reserve.ride_no = Ride.ride_no
-  AND Reserve.seats_needed > Ride.seats_available
-  RAISE EXCEPTION 'Not enough seats available in ride'
+  IF (Reserve.ride_no = Ride.ride_no
+  AND Reserve.seats_needed > Ride.seats_available) THEN
+  RAISE EXCEPTION 'Not enough seats available in ride';
+  END IF;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -77,10 +78,10 @@ CREATE TRIGGER TG_Not_enough_Seats
 
 CREATE FUNCTION TF_No_time_given() RETURNS TRIGGER AS $$
 BEGIN
-  WHEN Ride.earliest_time IS NULL
-  SET Ride.earliest_time = 00:00:00; -- unsure about semicolon
-  WHEN Ride.latest_time IS NULL
-  SET Ride.latest_time = 23:59:59;
+  WHEN (Ride.earliest_time IS NULL)
+  SET Ride.earliest_time = '00:00:00'; -- unsure about semicolon
+  WHEN (Ride.latest_time IS NULL)
+  SET Ride.latest_time = '23:59:59';
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
