9639b35fab180be86002ea738513946f0f40eed5
Meredith Brown <meredith.brown@duke.edu>
2020-03-05 12:35:45 -0500

    edited config.py

--- Flask/config.py
+++ Flask/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://vcm:dbpasswd@localhost/production'
+SQLALCHEMY_DATABASE_URI = 'postgresql://rideshare.dbpassword@vcm-13365.vm.duke.edu/production'
 SQLALCHEMY_ECHO = True
 DEBUG = True
