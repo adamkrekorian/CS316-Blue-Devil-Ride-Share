c017328cecd4675610d1f909135455f0bad591ee
Meredith Brown <meredith.brown@duke.edu>
2020-03-05 12:27:07 -0500

    changed database in config.py

--- Flask/config.py
+++ Flask/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://vcm:dbpasswd@localhost/beers' #definitely wrong
+SQLALCHEMY_DATABASE_URI = 'postgresql://vcm:dbpasswd@localhost/rideshare'
 SQLALCHEMY_ECHO = True
 DEBUG = True
