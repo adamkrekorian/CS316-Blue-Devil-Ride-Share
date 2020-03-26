a35744823aace0aeac50ff87342a72bae03580a7
Isabel Shiff <isabelshiff@gmail.com>
2020-03-26 14:31:51 -0400

    simple find rides implemented

--- Flask/app.py
+++ Flask/app.py
@@ -21,9 +21,8 @@ def home_page():
 
 @app.route('/find-rides', methods=('GET', 'POST'))
 def find_rides():
-
     form = forms.SearchFormFactory()
-    print("before first if")
+
     if 'logged_in' not in session:
         if session['logged_in']==False:
             flash("You are not logged in. Redirecting you to log in.")
@@ -34,30 +33,32 @@ def find_rides():
         return redirect(url_for('log_in'))     
 
     else:
-        print(form.errors)
-        print("before if in else")
+        print("errors: ", form.errors)
+
         if form.is_submitted():
             print("submitted")
-
-        if form.validate():
-            print("validated")
             origin_city = request.form['origin_city']
             destination = request.form['destination']
             date = request.form['date']
-            earliest_departure = request.form['earliest_departure']
-            latest_departure = request.form['latest_departure']
             spots_needed = request.form['spots_needed']
-        
-            results = db.session.query(models.Ride) \
-                .filter(models.Ride.origin == origin_city) \
-                .filter(models.Ride.destination == destination) \
-                .filter(models.Ride.date == date) \
-                .filter(models.Ride.earliest_time == earliest_departure) \
-                .filter(models.Ride.latest_time == latest_departure) \
-                .filter(models.Ride.seats_available <= spots_needed).all()
-            print("here: ")
-            print(results)
-        return render_template('find-rides.html', form=form)
+
+            if destination == "Search All":
+                print("search all")
+                results = db.session.query(models.Ride) \
+                    .filter(models.Ride.origin == origin_city) \
+                    .filter(models.Ride.date == date) \
+                    .filter(models.Ride.seats_available <= spots_needed).all()
+
+            else:
+                print("not search all")
+                results = db.session.query(models.Ride) \
+                    .filter(models.Ride.origin == origin_city) \
+                    .filter(models.Ride.destination == destination) \
+                    .filter(models.Ride.date == date) \
+                    .filter(models.Ride.seats_available <= spots_needed).all()
+            results = [x.__dict__ for x in results]
+            return render_template('find-rides.html', form=form, results = results)
+
     return render_template('find-rides.html', form=form)
 
 @app.route('/list-rides', methods=['GET','POST'])
